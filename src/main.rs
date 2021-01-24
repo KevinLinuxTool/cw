7e0e6f70939d4f5a5620a1b8d8beb816ba293301
KevinZonda <33132228+KevinZonda@users.noreply.github.com>
2021-01-24 13:42:30 +0000

    add time parser

--- src/main.rs
+++ src/main.rs
@@ -1,81 +1,44 @@
+extern crate chrono;
+
 use std::{env, io};
 use std::io::BufRead;
-use std::iter::FromIterator;
-use std::time::SystemTime;
+use chrono::Local;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
     let stdin = io::stdin();
     let stdout = io::stdout();
 
-    for line in stdin.lock().lines() {
-        let line = line.expect("NONE");
-    }
+    println!("{}", parse("{}{{%Y-%m-%d][%H:%M:%S}}{%Y-%m-%d %H:%M:%S}", "2333"))
+    //for line in stdin.lock().lines() {
+    //    let line = line.expect("NONE");
+    //}
 }
 
-fn parse<'a>(parse_str: &'a str, stdin_str: &'a str) -> &'a str {
-    // {yy}, {yyyy} -> year
-    // {m},  {mm} -> month
-    // {d},  {dd} -> day
-    let mut result: Vec<&str> = Vec::new();
+fn parse(parse_str: &str, stdin_str: &str) -> String {
+    let date = Local::now();
+    let mut result: Vec<String> = Vec::new();
     let split = parse_str.split("{{");
-    let mut index = 0;
-    let mut isFirst = true;
     for i in split {
         let ss = i.split("}}");
         for s in ss {
-            result.push(match s {
-                // TODO: FINISH LOGIC
-                "{}" => stdin_str,
-                _ => s
-            });
-            result.push("}");
+            result.push(
+                if s == "{}" {
+                    String::from(stdin_str)
+                } else if s.starts_with("{") && s.ends_with("}") {
+                    date.format(&s[1..s.len() - 1]).to_string()
+                } else {
+                    String::from(s)
+                });
+            result.push(String::from("}"));
         }
         result.pop();
-        result.push("{");
+        result.push(String::from("{"));
     }
     result.pop();
-
-    parse_str
-}
-
-#[derive(Debug)]
-struct Queue<T> {
-    data: Vec<T>,
-}
-
-impl<T> Queue<T> {
-    fn len(&self) -> usize {
-        self.data.len()
-    }
-
-    fn new() -> Self {
-        Queue { data: Vec::new() }
-    }
-
-    fn from(from: Vec<T>) -> Self {
-        Queue {
-            data: from
-        }
-    }
-
-    fn push(&mut self, item: T) {
-        self.data.push(item);
-    }
-
-    fn pop(&mut self) -> T {
-        self.data.remove(0)
-    }
-}
-
-impl Queue<char> {
-    fn to_string(&self) -> String {
-        self.data.iter().collect::<String>()
-    }
-
-    fn from_str(str: &str) -> Self {
-        Queue {
-            data: str.chars().collect::<Vec<_>>()
-        }
-    }
+    let mut result_str = String::new();
+    for rstr in result {
+        result_str.push_str(&rstr);
+    };
+    result_str
 }
